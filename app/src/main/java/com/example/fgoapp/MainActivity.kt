9a0e16e550b1cfec81760934a3209dd2635f25f9
Kamiruku <96325986+Kamiruku@users.noreply.github.com>
2022-12-15 20:56:44 +1300

    2022.12.15 Theme Updates

--- app/src/main/java/com/example/fgoapp/MainActivity.kt
+++ app/src/main/java/com/example/fgoapp/MainActivity.kt
@@ -1,15 +1,11 @@
 package com.example.fgoapp
 
-import android.content.Context
 import android.content.Intent
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import java.io.IOException
-import java.io.InputStream
 import java.nio.charset.Charset
 
 
